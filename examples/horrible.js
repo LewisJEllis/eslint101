17a92ed3dc7cdf61b60601fda476041970d26148
Piotr Jagielski <pjg@touk.pl>
2016-06-03 15:09:04 +0200

    eslint

--- examples/horrible.js
+++ examples/horrible.js
@@ -5,12 +5,11 @@ var a = 3;
 function eh(b,c){return b}
 
 var d ={
-  e: 'f', g: 'h',
-  i: 'j'};
+  e: 'f',g: 'h',
+  i:'j'};
 
 var underscore_name_hello = d['g'] + "ello"
 
-if ("hello" == underscore_name_hello) { console.log('equal!' ) ;
-}
+if ("hello" == underscore_name_hello) { console.log('equal!' ) ; }
 
 console['log']( underscore_name_hello );
